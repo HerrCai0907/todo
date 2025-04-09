c4acef8b2c256be309d24b8b22110c3d51a2716f
Congcong Cai <congcong.cai@bmw.com>
2025-04-09 11:51:26 +0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use clap::{Parser, Subcommand};
+use std::io::Write;
+
+#[derive(Debug, Parser)]
+#[command(propagate_version = true)]
+#[command(version, about = "todo command line tools")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    Add {},
+    Del {},
+    Done {},
+}
+
+fn read_input(content: &str) -> String {
+    let mut editor = rustyline::DefaultEditor::new().unwrap();
+    let readline = editor.readline(&(content.to_owned() + ": "));
+    match readline {
+        Ok(line) => {
+            if line.is_empty() {
+                line
+            } else {
+                line + &read_input(content)
+            }
+        }
+        Err(_) => String::new(),
+    }
+}
+
+fn add_task() -> () {
+    let task = read_input("task");
+    if task.is_empty() {
+        println!("No task provided.");
+        return;
+    }
+    let mut file = std::fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("todo.txt")
+        .unwrap();
+    writeln!(file, "- [] {}", task).unwrap();
+    println!("Task added: {}", task);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let m = Cli::parse();
+    match m.command {
+        Commands::Add {} => add_task(),
+        Commands::Del {} => todo!(),
+        Commands::Done {} => todo!(),
+    }
 }
