21d61b6aa0ef1506a61e826673d9792ba352e1e8
Congcong Cai <congcongcai0907@163.com>
2025-07-23 16:20:45 +0800

    feat: submit when click enter quickly

--- app/src/component/EditableLine.tsx
+++ app/src/component/EditableLine.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { PlusCircleOutlined } from "@ant-design/icons";
 import { Button, Flex, Input } from "antd";
 const { TextArea } = Input;
@@ -8,25 +8,34 @@ type P = {
   placeholder?: string;
   onSubmit: (text: string) => void;
 };
+// wasm perf 适配 perfetto
 
 const EditableLine: React.FC<P> = ({ initText, placeholder, onSubmit }) => {
   const [inputText, setInputText] = useState<string>(initText ?? "");
+  const lastEnterTime = useRef<number>(0);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInputText(e.target.value);
   };
-  const handleButtonClick = async () => {
+  const handleSubmit = async () => {
     const inputTextTrimmed = inputText.trim();
     // do not add empty task to avoid unintended click
     if (inputTextTrimmed.length == 0) return;
     onSubmit(inputTextTrimmed);
     setInputText("");
   };
+  const handlePressEnter = () => {
+    // when user click enter quickly, we want to submit the input
+    if (Date.now() - lastEnterTime.current <= 200) {
+      handleSubmit();
+    }
+    lastEnterTime.current = Date.now();
+  };
 
   return (
     <Flex vertical={false} align={"center"} gap={"small"}>
       <Button
-        onClick={handleButtonClick}
+        onClick={handleSubmit}
         type="primary"
         size="small"
         style={{ marginLeft: 8 }}
@@ -39,6 +48,7 @@ const EditableLine: React.FC<P> = ({ initText, placeholder, onSubmit }) => {
         allowClear
         onChange={handleInputChange}
         value={inputText}
+        onPressEnter={handlePressEnter}
       />
     </Flex>
   );
